fd6f90bf3d7d756343e391739b5dfe6f15db5184
Kevin Li <kevwkli@gmail.com>
2018-11-13 22:36:27 -0500

    first merge

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import x from 'X';
+import o from 'O';
+
+let gameWon = false;
+
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            board: Array(9).fill(null),
+            player: x
+        }
+    }
+    handleClick(index){
+        if (!gameWon) {
+            let newBoard = this.state.board;
+            if (this.state.board[index] === null) {
+                newBoard[index] = this.state.player;
+                this.setState({
+                    board: newBoard,
+                    player: this.state.player === x ? o : x
+                });
+            }
+            this.checkWinner();
+        }
+    }
+
+    checkWinner() {
+        let winLines = [
+            ["0","1","2"],
+            ["3","4","5"],
+            ["6","7","8"],
+            ["0","3","6"],
+            ["1","4","7"],
+            ["2","5","8"],
+            ["0","4","8"],
+            ["2","4","6"],
+        ]
+        for (let index = 0; index < winLines.length; index++) {
+            const [a, b, c] = winLines[index];
+            if (this.state.board[a] && this.state.board[a] === this.state.board[b] && this.state.board[a] === this.state.board[c]) {
+                gameWon = true;
+                alert("You won!");
+            }
+        }
+    }
+
+    render() {
+        const Box = this.state.board.map((box,index) => <div className="box" key={index} onClick={() => this.handleClick(index)}>{box}</div>);
+        return (
+            <div className="container">
+                <h1>Tic Tac Toe App</h1>
+                <div className="board">
+                    {Box}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
